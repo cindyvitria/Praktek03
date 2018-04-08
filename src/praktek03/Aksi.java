dad1a47187c6e16bba5ac9744edc4ab7596ba14d
cindyvitria <cindyvitria99@gmail.com>
2018-04-08 21:13:08 +0800

    Membuat instance, atribut dan memanggil Method

--- src/praktek03/Aksi.java
+++ src/praktek03/Aksi.java
@@ -11,6 +11,12 @@ package praktek03;
  */
 public class Aksi {
     public static void main(String[] args) {
+        Rumah Cendeh = new Rumah();
         
+        Cendeh.warna="Kuning";
+        Cendeh.bahan="Semen";
+        Cendeh.jmlkamar=4;
+        
+        Cendeh.cetakInfo();
     }
 }
